f5dd420f3ba2f29d35b2a06d45342f9eed3e1caf
Bluesman <dmitry.khalimov@gmail.com>
2020-09-03 20:21:09 +0300

    Работают фильтры

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 
 import SiteMenuView from "./view/site-menu.js";
-import FilterView from "./view/filter.js";
+
 import {generateTask} from "./mock/task.js";
 
 import BoardPresenter from "./presenter/board.js";
+import FilterPresenter from "./presenter/filter.js";
 import TasksModel from "./model/tasks.js";
 import FilterModel from "./model/filter.js";
 import {render, RenderPosition} from "./utils/render.js";
@@ -11,13 +12,7 @@ import {render, RenderPosition} from "./utils/render.js";
 const TASK_COUNT = 22;
 
 const tasks = new Array(TASK_COUNT).fill().map(generateTask);
-const filters = [
-  {
-    type: `all`,
-    name: `ALL`,
-    count: 0
-  }
-];
+
 
 const tasksModel = new TasksModel();
 tasksModel.setTasks(tasks);
@@ -27,9 +22,10 @@ const filterModel = new FilterModel();
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
-const boardPresenter = new BoardPresenter(siteMainElement, tasksModel);
-
 render(siteHeaderElement, new SiteMenuView(), RenderPosition.BEFOREEND);
-render(siteMainElement, new FilterView(filters, `all`), RenderPosition.BEFOREEND);
 
+const boardPresenter = new BoardPresenter(siteMainElement, tasksModel, filterModel);
+const filterPresenter = new FilterPresenter(siteMainElement, filterModel, tasksModel);
+
+filterPresenter.init();
 boardPresenter.init();
